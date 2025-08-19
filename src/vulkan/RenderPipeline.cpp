973ef86c862ad49346ee28b961f91dfc9e708ad0
Barbarisch <Barbarisch@users.noreply.github.com>
2025-08-19 15:24:50 -0400

    fixed z-fighting between static and dynamic cubes

--- src/vulkan/RenderPipeline.cpp
+++ src/vulkan/RenderPipeline.cpp
@@ -61,8 +61,8 @@ bool RenderPipeline::createGraphicsPipeline() {
     rasterizer.rasterizerDiscardEnable = VK_FALSE;
     rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
     rasterizer.lineWidth = 1.0f;
-    rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
-    rasterizer.frontFace = VK_FRONT_FACE_CLOCKWISE; // Changed to match our index buffer winding
+    rasterizer.cullMode = VK_CULL_MODE_FRONT_BIT; // Cull front faces instead of back
+    rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE; // Fixed to match vertex generation order
     rasterizer.depthBiasEnable = VK_FALSE;
 
     // Multisampling
@@ -230,7 +230,7 @@ bool RenderPipeline::createGraphicsPipelineForDynamicSubcubes() {
     rasterizer.rasterizerDiscardEnable = VK_FALSE;
     rasterizer.polygonMode = VK_POLYGON_MODE_FILL;
     rasterizer.lineWidth = 1.0f;
-    rasterizer.cullMode = VK_CULL_MODE_BACK_BIT;
+    rasterizer.cullMode = VK_CULL_MODE_FRONT_BIT; // Cull front faces instead of back
     rasterizer.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
     rasterizer.depthBiasEnable = VK_FALSE;
 
